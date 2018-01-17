ea2214aead11c2fb37bdfa9023b39f0cb5b4f705
Alex Ficklin and Jared Clemmensen <jared@>
2018-01-17 15:38:02 -0800

    Add CSS and JS to create math website.

---
+++ math/js/scripts.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+  $("#factorial").click(function(event) {
+    event.preventDefault();
+
+    var number1 = parseInt($("#number1").val());
+
+    var result = factorial(number1);
+
+    $("#result").text(result);
+
+    });
+  $("#palindrome").click(function(event) {
+    event.preventDefault();
+
+    var inputString = $("#number1").val();
+
+    if (palindrome(inputString)){
+      $("#result").text("That is a palindrome");
+    } else {
+      $("#result").text("That is not a palindrome");
+    }
+
+  });
+});
+
+var palindromeResult = function(parameter){
+  var j = parameter.length-1
+  for (var i=0 ; i<parameter.length/2 ; i++){
+    if (parameter.charAt(i)!=parameter.charAt(j)){
+      return false;
+    }
+    else {
+    j--;
+    }
+  } return true;
+}
+
+
+var factorialRecursive = function(parameter) {
+  if ((parameter-1) > 0) {
+    return result = parameter * factorial(parameter-1)
+  } else {
+    return 1;
+  }
+}
+
+var factorial = function(parameter) {
+  result = 1;
+  for (var i = 2; i <= parameter; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+var returnPrimes = function(parameter) {
+  //create array containing 2-parameter
+  var lessThan = [];
+  for (var i=0; i<parameter-1; i++) {
+    lessThan[i]=i+2;
+  }
+
+  //remove primes
+  for (var factor = 2; factor<parameter; factor++) {
+    console.log("factor:" + factor);
+    for (var j = 0; j<lessThan.length; j++) {
+      console.log("arrayval:" + lessThan[j]);
+      if ((lessThan[j]%factor)===0) {
+        lessThan.pop(j);
+      }
+    }
+  }
+  return lessThan;
+}
